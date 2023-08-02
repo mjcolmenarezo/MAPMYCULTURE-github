40dfe22ddb50ff83ca5cd7bd8c3efbf48e8fabb6
Maria Jose Colmenarez <marijocolmenarezo16@gmail.com>
2023-08-01 20:34:03 -0400

    Initial Commit

---
+++ MAPMYCULTURE/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MAPMYCULTURE
+//
+//  Created by MJ Colmenarez on 1/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MAPMYCULTURE/MAPMYCULTUREApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAPMYCULTUREApp.swift
+//  MAPMYCULTURE
+//
+//  Created by MJ Colmenarez on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MAPMYCULTUREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
